586c81ba50885b4c3470610e2c3a69c40af664bc
unknown <pchukwuemeka424@gmail.com>
2025-01-19 00:02:38 +0000

    first commit

--- src/app/product/layout.tsx
+++ src/app/product/layout.tsx
@@ -1,28 +1,29 @@
-import React from 'react'
-import Topnav from '@/components/topnav'
-import Head from 'next/head'
-import { Metadata } from 'next'
-
+import React, { ReactNode } from 'react';
+import Topnav from '@/components/topnav';
+import Head from 'next/head';
+import { Metadata } from 'next';
 
 export const metadata: Metadata = {
   title: `${process.env.APP_NAME}`,
   description: `${process.env.APP_DESCRIPTION}`,
+};
+
+interface LayoutProps {
+  children: ReactNode;
 }
 
-export default function layout({ children }) {
+export default function Layout({ children }: LayoutProps): JSX.Element {
   return (
-  <>
-  <Head>
-    <title>My Store</title>
-  </Head>
-       
-       <div className='mx-auto max-w-7xl'>
-          <Topnav />
+    <>
+      <Head>
+        <title>My Store</title>
+      </Head>
 
-          <div> {children} </div>
+      <div className='mx-auto max-w-7xl'>
+        <Topnav />
+
+        <div>{children}</div>
       </div>
-  </>
-    
-  
-  )
+    </>
+  );
 }
