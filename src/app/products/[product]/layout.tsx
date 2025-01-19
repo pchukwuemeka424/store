52677a2ae96f8e328d93ac9fd7816708827aae02
unknown <pchukwuemeka424@gmail.com>
2025-01-19 00:34:48 +0000

    first commit

--- src/app/products/[product]/layout.tsx
+++ src/app/products/[product]/layout.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
 import Topnav from '@/components/topnav';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: `${process.env.APP_NAME}`,
+  description: `${process.env.APP_DESCRIPTION}`,
+}
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
