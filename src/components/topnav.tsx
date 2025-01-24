d9aae769f8001d18580bdee37cca6aa0102c9098
unknown <pchukwuemeka424@gmail.com>
2025-01-24 21:37:30 +0000

    first commit

--- src/components/topnav.tsx
+++ src/components/topnav.tsx
@@ -56,7 +56,7 @@ export default function TopNav() {
               type="submit"
               className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2"
             >
-              Search
+              <FaSearch className="w-4 h-4" /> {/* Search Icon on Button */}
             </button>
           </div>
         </form>
@@ -95,7 +95,7 @@ export default function TopNav() {
                 type="submit"
                 className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2"
               >
-                Search
+                <FaSearch className="w-4 h-4" /> {/* Search Icon on Button */}
               </button>
             </div>
           </form>
