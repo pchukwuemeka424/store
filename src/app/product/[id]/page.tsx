889a3e271ee32874895e1afb5e15896f78901d2d
unknown <pchukwuemeka424@gmail.com>
2025-02-01 13:54:35 +0000

    first co

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -2,14 +2,35 @@
 import React from 'react';
 import FetchSingleProduct from './fetchSingle';
 import { Metadata } from 'next';
+import { createClient } from '@/utils/supabase/client';
 
 export async function generateMetadata({ params }: { params: { id: string } }): Promise<Metadata> {
+    const supabase = createClient();
+  
+  const { data, error } = await supabase
+    .from('products')
+    .select('*')
+    .eq('user_id', params.id)
+    .single();
+
+  const user = data ?? {};
   return {
-    title: `${process.env.APP_NAME} - ${params.id}`,
-    description: process.env.APP_DESCRIPTION || '',
+    title: `${process.env.APP_NAME} - ${user.title || 'Store'}`,
+    description: `${user.description || ''}`,
+    openGraph: {
+      title: `${process.env.APP_NAME} - ${user.title || 'Store'}`,
+      description: `${user.description || ''}`,
+      images: [
+        {
+          url: `${user.image}`,
+          width: 800,
+          height: 600,
+          alt: 'Og Image Alt',
+        },
+      ],
+    },
   };
 }
-
 // Page Component
 export default function Page() {
 
--- src/app/store/[store]/page.tsx
+++ src/app/store/[store]/page.tsx
@@ -1,12 +1,34 @@
 import React from 'react';
-
 import Shop from './shop';
 import { Metadata } from 'next';
+import { createClient } from '@/utils/supabase/client';
 
 export async function generateMetadata({ params }: { params: { store: string } }): Promise<Metadata> {
+  const supabase = createClient();
+
+  const { data, error } = await supabase
+    .from('user_profile')
+    .select('*')
+    .eq('username', params.store)
+    .single();
+
+  const user = data ?? {}; // Ensure user is always an object
+
   return {
-    title: `${process.env.APP_NAME} - ${params.store}`,
+    title: `${process.env.APP_NAME} - ${user.shopname || 'Store'}`,
     description: process.env.APP_DESCRIPTION || '',
+    openGraph: {
+      title: `${process.env.APP_NAME} - ${user.shopname || params.store}`,
+      description: process.env.APP_DESCRIPTION || '',
+      images: [
+        {
+          url: `${user.avater}`,
+          width: 800,
+          height: 600,
+          alt: 'Og Image Alt',
+        },
+      ],
+    },
   };
 }
 
