05f4e3803320b53a49b4cadb7a5e7452eaafdfff
unknown <pchukwuemeka424@gmail.com>
2025-02-01 11:13:02 +0000

    first co

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,13 +1,22 @@
+
 import React from 'react';
 import FetchSingleProduct from './fetchSingle';
+import { Metadata } from 'next';
 
-
-// Page Component
-export default async function Page() {
-  return (
-    <div>
-      <FetchSingleProduct />
-    </div >
-  );
+export async function generateMetadata({ params }: { params: { id: string } }): Promise<Metadata> {
+  return {
+    title: `${process.env.APP_NAME} - ${params.id}`,
+    description: process.env.APP_DESCRIPTION || '',
+  };
 }
 
+// Page Component
+export default function Page() {
+
+
+  return (
+    <div>
+      <FetchSingleProduct/>
+    </div>
+  );
+}
--- src/app/store/[store]/page.tsx
+++ src/app/store/[store]/page.tsx
@@ -1,8 +1,14 @@
-'use client';
-
 import React from 'react';
 
 import Shop from './shop';
+import { Metadata } from 'next';
+
+export async function generateMetadata({ params }: { params: { store: string } }): Promise<Metadata> {
+  return {
+    title: `${process.env.APP_NAME} - ${params.store}`,
+    description: process.env.APP_DESCRIPTION || '',
+  };
+}
 
 export default function Page() {
   return (
