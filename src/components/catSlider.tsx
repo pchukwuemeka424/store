e382bd5aaa07312512ed0f6ba70597fc653b2166
unknown <pchukwuemeka424@gmail.com>
2025-02-05 14:04:42 +0000

    first co

--- src/components/catSlider.tsx
+++ src/components/catSlider.tsx
@@ -33,7 +33,7 @@ export default function SliderComponent() {
     responsive: [
       { breakpoint: 1024, settings: { slidesToShow: 4 } },
       { breakpoint: 768, settings: { slidesToShow: 3 } },
-      { breakpoint: 480, settings: { slidesToShow: 2 } },
+      { breakpoint: 480, settings: { slidesToShow: 3 } },
     ],
   };
 
