dfbe96a496be22466da7462f6ea559dc50fd2c6e
unknown <pchukwuemeka424@gmail.com>
2025-02-07 06:56:00 +0000

    first co

--- src/components/ProductNotFound.tsx
+++ src/components/ProductNotFound.tsx
@@ -1,12 +1,20 @@
+import Link from 'next/link';
 import React from 'react';
-// Import the icon you want to use
 import { FaShopLock } from 'react-icons/fa6';
 
 const ProductNotFound = () => {
   return (
-    <div className="bg-white flex flex-col items-center justify-center absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-gray-100">
-      <h1 className="text-4xl font-bold mb-4">Product Not Found</h1>
-      <FaShopLock size={48} color="gray" /> {/* Display the empty shop icon */}
+    <div className="flex items-center justify-center min-h-screen bg-gray-100 px-4">
+      <div className="bg-white shadow-lg rounded-2xl p-8 max-w-md text-center transform transition duration-300 hover:scale-105">
+        <div className="flex justify-center items-center bg-gray-200 p-4 rounded-full mb-4">
+          <FaShopLock className="text-gray-600" size={64} />
+        </div>
+        <h1 className="text-3xl font-semibold text-gray-800 mb-2">Product Not Found</h1>
+        <p className="text-gray-600 mb-6">We couldn’t find the product you’re looking for. Try searching again or browse other categories.</p>
+        <Link href="/" className="text-blue-600 hover:underline">
+        <button className="px-6 py-3 bg-blue-600 text-white rounded-lg shadow-md hover:bg-blue-700 transition">Go Back</button>
+        </Link>
+      </div>
     </div>
   );
 };
