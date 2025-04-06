6715bea2f75be59721b171fa11c88f0f59803dcc
Prince chukwuemeka <113414178+Hoakbxb@users.noreply.github.com>
2025-04-06 15:46:33 +0100

    first co

--- next-sitemap.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-    siteUrl: 'https://afrivendor.ng', // Replace with your actual domain
-    generateRobotsTxt: true, // Generates robots.txt file
-  };
-  
