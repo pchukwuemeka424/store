9bd0e61940d878036742a349b64aad27254aafee
Prince chukwuemeka <162118418+pchukwuemeka424@users.noreply.github.com>
2025-04-06 08:47:29 +0100

    Create next-sitemap.config.js

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://afrivendor.ng', // Replace with your actual domain
+    generateRobotsTxt: true, // Generates robots.txt file
+  };
+  
